a2074a209d3bb8e18c8059952dd2a14a255f2161
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-09-02 13:13:07 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,18 @@
-//import 'redux'
-// @ts-expect-error fail TS2664
+// import 'redux'
+
+// eslint-disable-next-line
+interface Action {}
+
+// eslint-disable-next-line
+interface AnyAction extends Action {}
+
+// eslint-disable-next-line
+type ThunkAction< A, B, C, D> = A | B | C | D
+
+// @ts-expect-error TS2664
 declare module 'redux' {
-  type Dispatch<A extends Action = AnyAction> = <S, E, R>(asyncAction: ThunkAction<R, S, E, A>) => R
+  interface Dispatch<A extends Action = AnyAction> {
+    <S, E, R>(asyncAction: ThunkAction<R, S, E, A>): R
+  }
 }
+
