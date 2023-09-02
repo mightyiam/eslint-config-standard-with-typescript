600aecd9cf5f6e9153451b6236546f3183920f02
jay-bulk <jay.bulk@proton.me>
2023-09-01 23:58:36 -0600

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
-import 'redux'
-
+//import 'redux'
+// @ts-expect-error fail TS2664
 declare module 'redux' {
   type Dispatch<A extends Action = AnyAction> = <S, E, R>(asyncAction: ThunkAction<R, S, E, A>) => R
 }
