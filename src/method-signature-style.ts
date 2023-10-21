a81e0daed0fa1bd08dea337de1d009da277f1c6f
jay-bulk <jay.bulk@proton.me>
2023-10-21 01:44:57 -0600

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/method-signature-style.ts

--- src/method-signature-style.ts
+++ src/method-signature-style.ts
@@ -1,36 +1,34 @@
 interface Logger {
-  log: (s: string) => void 
+  log: (s: string) => void
 }
 
-type ProtoConstructor = new () => ProtoInstance
+type LoggerConstructor = new () => Logger
 
-interface AInstance extends ProtoInstance { // Sealed 
-  load: (s: string) => Promise<void>
+interface RequestLoggerInstance extends Logger { // Sealed 
+  log: (s: string) => Promise<void>
 }
 
-type AConstructor = new () => AInstance
-
-export class Proto implements ProtoInstance {
-  async load (s: string): Promise<void> {
-    console.log('proto load', s)
+export class RequestLoggerClass implements RequestLoggerInstance {
+  async log(s: string): Promise<void> {
+    console.log('REQUEST:', s)
   }
 }
 
-export class Proto implements ProtoInstance {
-  async load (s: string): Promise<void> {
-    console.log('proto load', s)
+export class ResponseLogger implements RequestLoggerInstance {
+  async log(s: string): Promise<void> {
+    console.log('proto log', s)
   }
 }
 
 
-export function getConstructor (Base: ProtoConstructor): AConstructor {
+export function getConstructor(Base: ProtoConstructor): AConstructor {
   return class A extends Base {
-    // will say "TS2425: Class 'ProtoInstance' defines instance member property 'load',
+    // will say "TS2425: Class 'ProtoInstance' defines instance member property 'log',
     // but extended class 'A' defines it as instance member function."
-    async load (s: string): Promise<void> {
-      console.log('a load', s)
+    async log(s: string): Promise<void> {
+      console.log('a log', s)
 
-      return await super.load(s)
+      return await super.log(s)
     }
   }
 }
@@ -46,35 +44,35 @@ NodeStream = ProtoInstance
 
 
 interface ProtoInstance {
-  load: (s: string) => Promise<void>
+  log: (s: string) => Promise<void>
 }
 
 abstract class AClass implements ProtoInstance {
-  async load (s: string): Promise<void>  { 
+  async log(s: string): Promise<void> {
     throw new Error('should be implemented in inherided class')
   }
 }
 
 abstract class AbstractProto implements ProtoInstance {
-  async load (s: string): Promise<void>  { 
+  async log(s: string): Promise<void> {
     throw new Error('should be implemented in inherided class')
   }
 
-  async otherPrivateMethod() {}
+  async otherPrivateMethod() { }
 }
 
 export class Proto extends AbstractProto implements ProtoInstance {
-  async load (s: string): Promise<void> {
-    console.log('proto load', s)
+  async log(s: string): Promise<void> {
+    console.log('proto log', s)
   }
 }
 
-export function getConstructor (Base: typeof AbstractProto): typeof AClass {
+export function getConstructor(Base: typeof AbstractProto): typeof AClass {
   return class A extends Base {
-    async load (s: string): Promise<void> {
-      console.log('a load', s)
+    async log(s: string): Promise<void> {
+      console.log('a log', s)
       this.otherPrivateMethod()
-      return await super.load(s)
+      return await super.log(s)
     }
   }
 }
@@ -82,89 +80,3 @@ export function getConstructor (Base: typeof AbstractProto): typeof AClass {
 /**otherPrivateMethod is not callable here */
 getConstructor()
 
-```
-
-
-
-<details><summary><b>Output</b></summary>
-
-```ts
-Image;
-export class Proto {
-    async load(s) {
-        console.log('proto load', s);
-    }
-}
-export class Proto {
-    async load(s) {
-        console.log('proto load', s);
-    }
-}
-export function getConstructor(Base) {
-    return class A extends Base {
-        // will say "TS2425: Class 'ProtoInstance' defines instance member property 'load',
-        // but extended class 'A' defines it as instance member function."
-        async load(s) {
-            console.log('a load', s);
-            return await super.load(s);
-        }
-    };
-}
-/// ---------------------------
-NodeStream = ProtoInstance;
-class AClass {
-    async load(s) {
-        throw new Error('should be implemented in inherided class');
-    }
-}
-class AbstractProto {
-    async load(s) {
-        throw new Error('should be implemented in inherided class');
-    }
-    async otherPrivateMethod() { }
-}
-export class Proto extends AbstractProto {
-    async load(s) {
-        console.log('proto load', s);
-    }
-}
-export function getConstructor(Base) {
-    return class A extends Base {
-        async load(s) {
-            console.log('a load', s);
-            this.otherPrivateMethod();
-            return await super.load(s);
-        }
-    };
-}
-/**otherPrivateMethod is not callable here */
-getConstructor();
-
-```
-
-
-</details>
-
-
-<details><summary><b>Compiler Options</b></summary>
-
-```json
-{
-  "compilerOptions": {
-    "strict": true,
-    "noImplicitAny": true,
-    "strictNullChecks": true,
-    "strictFunctionTypes": true,
-    "strictPropertyInitialization": true,
-    "strictBindCallApply": true,
-    "noImplicitThis": true,
-    "noImplicitReturns": true,
-    "alwaysStrict": true,
-    "esModuleInterop": true,
-    "declaration": true,
-    "target": "ES2017",
-    "jsx": "react",
-    "module": "ESNext",
-    "moduleResolution": "node"
-  }
-}
