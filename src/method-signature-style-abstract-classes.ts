5ef82fab279160d79615671134731e9b4b4e989e
jay-bulk <jay.bulk@proton.me>
2023-10-27 23:59:17 -0600

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/method-signature-style-abstract-classes.ts

--- src/method-signature-style-abstract-classes.ts
+++ src/method-signature-style-abstract-classes.ts
@@ -3,27 +3,27 @@ interface StoreInterface {
 }
 
 abstract class AbstractStore implements StoreInterface {
-  write (_data: string): void {
+  write(_data: string): void {
     throw new Error('not implemented')
   }
 }
 
 class DbStore extends AbstractStore {
-  write (_data: string): void {
+  write(_data: string): void {
     // write to db
   }
 }
 
-class FsStore implements AbstractStore {
-  write (_data: string): void {
+class FsStore extends AbstractStore {
+  write(_data: string): void {
     // write to fs
   }
 }
 
-function extendStoreClass (BaseStore: new () => StoreInterface, debug: boolean): (new () => StoreInterface) {
+function extendStoreClass(BaseStore: new () => AbstractStore, debug: boolean): (new () => StoreInterface) {
   if (debug) {
     return class StoreLoggerMixin extends BaseStore {
-      write (data: string): void {
+      write(data: string): void {
         console.log('storing: ', data)
         super.write(data)
       }
