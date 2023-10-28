d5164007e6a43c497a369a83fb498790f136c059
jay-bulk <jay.bulk@proton.me>
2023-10-27 23:30:22 -0600

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/method-signature-style.ts

--- src/method-signature-style.ts
+++ src/method-signature-style.ts
@@ -17,9 +17,9 @@ class FsStore implements StoreInterface {
 export function extendStoreClass (BaseStore: new () => StoreInterface, debug: boolean): (new () => StoreInterface) {
   if (debug) {
     return class StoreLoggerMixin extends BaseStore {
-      write(data: string): void {
+      write (data: string): void {
         console.log('storing: ', data)
-        super.write('')
+        super.write(data)
       }
     }
   }
@@ -48,6 +48,53 @@ store.write('some data')
 
 // /// ---------------------------
 
+interface StoreInterface {
+  write: (data: string) => void
+}
+
+class DbStore implements StoreInterface {
+  write (_data: string): void {
+    // write to db
+  }
+}
+
+class FsStore implements StoreInterface {
+  write (_data: string): void {
+    // write to fs
+  }
+}
+
+export function extendStoreClass (BaseStore: new () => StoreInterface, debug: boolean): (new () => StoreInterface) {
+  if (debug) {
+    return class StoreLoggerMixin extends BaseStore {
+      write (data: string): void {
+        console.log('storing: ', data)
+        super.write(data)
+      }
+    }
+  }
+
+  return BaseStore
+}
+
+let StoreClass: new () => DbStore
+let store: StoreInterface
+
+StoreClass = extendStoreClass(DbStore, false)
+store = new StoreClass()
+store.write('some data')
+
+StoreClass = extendStoreClass(DbStore, true)
+store = new StoreClass()
+store.write('some data')
+
+StoreClass = extendStoreClass(FsStore, false)
+store = new StoreClass()
+store.write('some data')
+
+StoreClass = extendStoreClass(FsStore, true)
+store = new StoreClass()
+store.write('some data')
 // NodeStream = ProtoInstance
 
 // interface ProtoInstance {
