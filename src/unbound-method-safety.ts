3f6242bda64f3100af458284999d524f74c0282e
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-11-04 12:23:37 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/unbound-method-safety.ts

--- src/unbound-method-safety.ts
+++ src/unbound-method-safety.ts
@@ -1,8 +1,19 @@
-interface MethodStyle {
+interface MethodStyleIface {
   // Shorthand method signature is forbidden.
+  method (): void
+}
+
+interface PropertyStyleIface {
   method: () => void
 }
 
-interface PropertyStyle {
-  method: () => void
+class MethodStyle implements MethodStyleIface {
+  method (): void {
+    console.log(this.method)
+  }
+
 }
+
+const methodStyle = new MethodStyle()
+const methodStyleMethod = methodStyle.method
+methodStyleMethod()
