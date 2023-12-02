7138afa6d33ad8dcd8d8628db0abc451d56d8296
Shahar "Dawn" Or <mightyiampresence@gmail.com>
2023-12-02 14:06:55 +0700

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/unbound-method.ts

--- src/unbound-method.ts
+++ src/unbound-method.ts
@@ -1,26 +1,26 @@
 class Animal {
-  // sound (): void {
-  //   throw new Error()
-  // }
+  sound (): void {
+    throw new Error()
+  }
 
   static population (n: number): Animal[] {
     return Array(n).fill(null).map(() => new Animal())
   }
 }
 
-let { population } = Animal
+export const { population } = Animal
 
+export class Dog extends Animal {
+  size: number = 0
+  sound (): void {
+    console.log('bark' + (this.size < 2 ? ' loud' : ''))
+  }
+}
 
-// export class Dog extends Animal {
-//   sound (): void {
-//     console.log('bark' + (this.size < 2 ? ' loud' : ''))
-//   }
-// }
-//
-// export const registerListerner = (callback: () => void): void => {
-//
-// }
+export const registerListerner = (callback: () => void): void => {
 
-// const a: Animal = new Dog()
+}
 
-// registerListerner(a.sound) // Error: this is undefined
+const a: Animal = new Dog()
+
+registerListerner(a.sound) // Error: this is undefined
